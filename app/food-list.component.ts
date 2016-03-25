1804b8cc1e768d2bf9f7bba7a0b5d9edff98c3f0
Matt Rosanio <mmrosani@syr.edu>
2016-03-25 08:40:26 -0700

    Add working new food component

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,16 +1,18 @@
 import { Component } from 'angular2/core';
 import { Food } from './food.model';
 import { FoodInfoComponent } from './food-info.component';
+import { NewFoodComponent } from './new-food.component';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
-  directives: [FoodInfoComponent],
+  directives: [FoodInfoComponent, NewFoodComponent],
   template: `
     <div *ngFor="#currentFood of foodList">
       <h3 (click)="clickFood(currentFood)">{{currentFood.name}}</h3>
       <food-info *ngIf="selectedFood === currentFood" [food]="currentFood"></food-info>
     </div>
+    <new-food (onSubmitNewFood)="createFood($event)"></new-food>
   `
 })
 
@@ -23,5 +25,8 @@ export class FoodListComponent {
     } else {
       this.selectedFood = clickedFood;
     }
+  }
+  createFood(foodArray: Array<any>) {
+    this.foodList.push(new Food(foodArray[0], foodArray[1], foodArray[2]));
   }
 }
---
+++ app/new-food.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Food } from './food.model';
+
+@Component({
+  selector: 'new-food',
+  outputs: ['onSubmitNewFood'],
+  template: `
+    <div class="col-xs-6">
+      <h2>Log a new food:</h2>
+      <input type="text" placeholder="Name" class="form-control" #newName>
+      <input type="text" placeholder="Details" class="form-control" #newDetails>
+      <input type="number" placeholder="Calories" class="form-control" #newCalories>
+      <button (click)="createFood(newName, newDetails, newCalories)" class="btn btn-primary">Go!</button>
+    </div>
+  `
+})
+
+export class NewFoodComponent {
+  public onSubmitNewFood: EventEmitter<any>
+  constructor() {
+    this.onSubmitNewFood = new EventEmitter();
+  }
+  createFood(userName: HTMLInputElement, userDetails: HTMLInputElement, userCalories: HTMLInputElement) {
+    var foodArray: Array<any> = [userName.value, userDetails.value, userCalories.value];
+    this.onSubmitNewFood.emit(foodArray);
+    userName.value = "";
+    userDetails.value = "";
+    userCalories.value = "";
+  }
+}
