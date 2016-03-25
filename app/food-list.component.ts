a1da3262da72a99c0f1c6155b3b202ac2b538d4f
Matt Rosanio <mmrosani@syr.edu>
2016-03-25 08:24:14 -0700

    add functionality to display details and calories of food when clicked on

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -8,12 +8,20 @@ import { FoodInfoComponent } from './food-info.component';
   directives: [FoodInfoComponent],
   template: `
     <div *ngFor="#currentFood of foodList">
-      <h3>{{currentFood.name}}</h3>
-      <food-info [food]="currentFood"></food-info>
+      <h3 (click)="clickFood(currentFood)">{{currentFood.name}}</h3>
+      <food-info *ngIf="selectedFood === currentFood" [food]="currentFood"></food-info>
     </div>
   `
 })
 
 export class FoodListComponent {
   public foodList: Food[];
+  public selectedFood: Food;
+  clickFood(clickedFood: Food) {
+    if(this.selectedFood === clickedFood) {
+      this.selectedFood = undefined;
+    } else {
+      this.selectedFood = clickedFood;
+    }
+  }
 }
